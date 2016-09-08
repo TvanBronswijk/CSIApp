516dcbe667584e43a3613ebc66651dac36fa273a
Tobi <tvanbronswijk@gmail.com>
2016-09-08 11:21:20 +0200

    commit2

--- app/src/main/java/school/csiapp/ReportActivity.java
+++ app/src/main/java/school/csiapp/ReportActivity.java
@@ -1,25 +1,23 @@
 package school.csiapp;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ReportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_report);
 
-        Button button = (Button)findViewById(R.id.button);
+        Button button = (Button)findViewById(R.id.exit);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              Intent intent = new Intent(MainActivity.this, ReportActivity.class);
-                startActivity(intent);
+                ReportActivity.this.finish();
             }
         });
     }
